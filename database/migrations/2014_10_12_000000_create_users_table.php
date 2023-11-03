a756902b7676847af57ea4df21ff4afe06a92bea
Wycliffe Nyachiro <georgewycs@gmail.com>
2023-11-03 09:54:25 +0300

    Members Model and  ReadMe Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('active')->default('YES');
+            $table->boolean('active')->default(true);
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
